ba014450ad33acd123b91c6c5e84d19c66b0be95
AndrewZ-0 <107640838+AndrewZ-0@users.noreply.github.com>
2024-11-10 22:28:20 +0000

    Delete objects directory

--- objects/objects.js
+++
@@ -1,8 +0,0 @@
-import {Cube} from "./cube.js";
-import {Torus} from "./torus.js";
-import {Sphere} from "./sphere.js";
-import {Cylinder} from "./cylinder.js";
-
-export {Cube, Torus, Sphere, Cylinder};
-
-
